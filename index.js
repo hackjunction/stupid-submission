0521b2bd084e16b9180c1e63ed6e8ea132b69da5
Henrik Aarnio <hjaarnio@gmail.com>
2018-04-14 08:33:02 +0300

    set port properly

--- index.js
+++ index.js
@@ -171,4 +171,4 @@ app.get('/api', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port ' + port))
